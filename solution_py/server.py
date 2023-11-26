a111d59ce8875d57f746334d37968319501c9f34
mwroblew <01161701@pw.edu.pl>
2023-11-26 20:10:24 +0100

    added checksum

--- solution_py/server.py
+++ solution_py/server.py
@@ -1,4 +1,5 @@
 import socket
+from zlib import adler32 as a32
 
 
 def main():
@@ -14,17 +15,25 @@ def main():
         data, addr = server_socket.recvfrom(1024)  # Odbieranie danych z klienta
         if verify_data(data):  # Weryfikacja odebranych danych
             response = b"OK"
-            server_socket.sendto(response, addr)  # Odsyłanie odpowiedzi do klienta
             print(f"Odebrano poprawne dane od {addr}")
         else:
+            response = b"FAIL"
             print(f"Odebrano niepoprawne dane od {addr}")
+        server_socket.sendto(response, addr)  # Odsyłanie odpowiedzi do klienta
+
+
+def verify_checksum(data):
+    # print(data[2:6])
+    # print(a32(data[6:]).to_bytes(4, byteorder="big"))
+    return (data[2:6] == a32(data[6:]).to_bytes(4, byteorder="big"))
 
 
 def verify_data(data):
+    print(data)
     # Weryfikacja długości datagramu i zawartości
-    if len(data) >= 2 and data[:2] == len(data[2:]).to_bytes(2, byteorder="big"):
-        # print(data)
-        return True
+    if len(data) >= 2 and data[:2] == len(data[6:]).to_bytes(2, byteorder="big"):
+        if verify_checksum(data):
+            return True
     return False
 
 
