71598b530ceb9ce804f9191615f0342b9bd17e88
mwroblew <01161701@pw.edu.pl>
2023-12-14 13:12:37 +0100

    pythonserverwork

--- task2/2.1/python/server.py
+++ task2/2.1/python/server.py
@@ -8,17 +8,42 @@ HOST_DEFAULT = '0.0.0.0'
 BUFSIZE = 512
 
 
-def unpack_data(data):
+import struct
+
+def unpack_int(data):
     # Unpack the received data
-    unpacked_data = struct.unpack('!H L 20s', data)
-    short_num, int_num, string_data = unpacked_data
-    return short_num, int_num, string_data.decode('utf-8').rstrip('\x00')
+    list_length = struct.unpack('<I', data)[0]
+    return list_length
+
+
+def unpack_str(data):
+    # Unpack the received data
+    list_length = struct.unpack('<S', data)[0]
+    return list_length
+
+
+def receive_linked_list(client_socket, list_length):
+    linked_list = []
+    for _ in range(list_length):
+        node = receive_node(client_socket)
+        linked_list.append(node)
+    return linked_list
+
+def receive_node(client_socket):
+    _ = client_socket.recv(4)
+    short_int = client_socket.recv(2)
+    long_int = unpack_int(client_socket.recv(4))
+    fixed_string = client_socket.recv(4)
+    string_length = unpack_int(client_socket.recv(4))
+    dynamic_string = client_socket.recv(string_length)
+
+    return (short_int, long_int, fixed_string, string_length, dynamic_string)
 
 def main():
     if len(sys.argv) < 3:
-        print(f"Using :{port}")
         host = HOST_DEFAULT
         port = 8000
+        print(f"Using :{port}")
     else:
         host = sys.argv[1]
         port = int(sys.argv[2])
@@ -35,13 +60,15 @@ def main():
             print(f"Connection from {addr}")
 
             # Receive data
-            data = client_socket.recv(BUFSIZE)
-            if not data:
+            list_length_data = client_socket.recv(4)
+            if not list_length_data:
                 break
+            list_length = unpack_int(list_length_data)
 
-            # Unpack and print data
-            short_num, int_num, string_data = unpack_data(data)
-            print(f"Received data: Short Num: {short_num}, Int Num: {int_num}, String: {string_data}")
+            # Receive linked list
+            linked_list = receive_linked_list(client_socket, list_length)
+            print("Received linked list:")
+            print(linked_list)
         
 
 if __name__ == "__main__":
