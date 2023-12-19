528b80d351a0484c0d49908d8cb526398820f55d
atorzews <01169253@pw.edu.pl>
2023-12-19 11:15:42 +0100

    Task 2.2 done

---
+++ task2/2.2/c/client.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <byteswap.h>
+#include <string.h>
+
+#include "networking/networking.h"
+#include "data/linked_list.h"
+
+int main(int argc, char *argv[])
+{
+    uint32_t list_length = 5;
+
+    if (argc != 3) {
+        printf("usage: hostname port\n");
+        return 0;
+    }
+
+    struct socketInfo socketInfo = getSocket(argv[2], argv[1]);
+
+    if (socketInfo.sockfd == -1)
+    {
+        printf("Failed to create socket, shutting down...\n");
+        close(socketInfo.sockfd);
+        return -1;
+    }
+
+    printf("Connected to server, sending data...\n");
+
+    uint32_t buf[1];
+    __bswap_32(list_length); // convert to big endian
+    buf[0] = list_length;
+
+    if (send(socketInfo.sockfd, buf, sizeof(uint32_t), 0) != -1)
+        printf("Sent list length!\n");
+    else
+    {
+        printf("Error in sending list length, shutting down...\n");
+        close(socketInfo.sockfd);
+        return -1;
+    }
+
+    node* linked_list = create_linked_list(list_length);
+    node* current_node = linked_list;
+    int i = 0;
+
+    while (current_node != NULL)
+    {
+        printf("id: %d, short_val: %d, int_val: %d, fixed_string: %s, dynamic_string: %s \n",
+               i, current_node->short_val, current_node->int_val, current_node->fixed_string, current_node->dynamic_string);
+
+        if (send(socketInfo.sockfd, current_node,
+                 sizeof(node)+ current_node->dynamic_string_length, 0) != -1)
+        {
+            printf("Sent node of id %d\n", i);
+        }
+        else
+        {
+            printf("Error in sending node, shutting down...\n");
+            delete_linked_list(linked_list);
+            close(socketInfo.sockfd);
+            return -1;
+        }
+        current_node = current_node->next;
+        i++;
+    }
+
+    printf("Sent the linked list!\n");
+
+    delete_linked_list(linked_list);
+    close(socketInfo.sockfd);
+
+    return 0;
+}
+
