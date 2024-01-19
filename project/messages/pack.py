ff17719e5b22645cf0327337f1aa11416a42e9ee
atorzews <01169253@pw.edu.pl>
2024-01-19 22:40:15 +0100

    Implemeted PEERS unpack

--- project/messages/pack.py
+++ project/messages/pack.py
@@ -88,6 +88,9 @@ def unpack(data: bytes, msg_type: MsgType) -> message_body_t:
 
         case MsgType.ERROR:
             return ERROR_body._make(unpacked)
+        
+        case MsgType.PEERS:
+            return PEERS_body._make(unpacked)
 
         case _:
             raise NotImplementedError("Invalid message type or not implemented")
